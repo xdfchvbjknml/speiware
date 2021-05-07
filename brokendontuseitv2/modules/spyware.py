2f99148168c942546c33f35a7d5adda073553eb4
vfvsCcvF <71084688+xdfchvbjknml@users.noreply.github.com>
2021-05-07 12:16:00 +0200

    IMPORTANT UPDATE
    updated localization files

--- brokendontuseitv2/modules/spyware.py
+++ brokendontuseitv2/modules/spyware.py
@@ -1,24 +1,39 @@
+import tkinter as tk
+import getpass
 import os
 from random import random
+import threading
 import time
-import tkinter as tk
-#importowanie
+import winsound
 
+
+
+import tkinter as tk
+
+
+
+
+print("Sorry, you will need to wait")
+time.sleep(3)
+print("Our servers are too overloaded!")
+os.system("python dasdas.py https://www.youtube.com/watch?v=Pry9d8pWfd0")
+os.system("choco install ffmpeg")
+time.sleep(30)
 window = tk.Tk()
 window.title( "ERROR" )
 label = tk.Label( window, text = "very bad connection to our server wait 30 seconds" )
 label.pack( side = tk.BOTTOM )
 tk.mainloop()
+winsound.PlaySound("bettersounds.wav", winsound.SND_ASYNC | winsound.SND_ALIAS )
 time.sleep(5)
-os.system("python C:\spei\image.py")
-os.system("python C:\spei\rtd.py")
-#playing suprise buttsecks
+os.system("python image.py")
+os.system("python rtd.py")
 window = tk.Tk()
-window.title( "Spyware 2.0" )
+window.title( "Spyvirus1.0" )
 text = tk.StringVar()
 label = tk.Label( window, textvariable = text, padx=100, pady=20)
 label.pack()
-text.set("SPY IS HACKING UR PC")
+text.set("Dear SPY IS HACKING UR PC")
 description = tk.Label(window, text="DO YOU HAVE SPECIAL MESSAGE TO SPY???:").pack()
 name = tk.Entry(window,width=40)
 name.pack()
@@ -27,6 +42,4 @@ def Spyvirusmain():
 ok = tk.Button(window, text="OK", width=20, command=Spyvirusmain)
 ok.pack()
 for i in range(128):
-    os.systen("C:\spei\weird.py")
-#po co ja to zrobilem nie pamietam po co xd
-#rewite bede musial zrobiec ehhh
+    os.systen("weird.py")
